ae7bbd783d4cd0a37dbfb32673971997d2d182b7
Dominic Tracy <spoon@Dominics-MacBook-Air.local>
2023-06-17 22:53:53 -0500

    first commit but had to redo repo because terminal made me mad

---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useState } from "react";
+
+import SearchManufacturer from "./SearchManufacturer";
+
+const handleSearch = () => {
+
+}
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('');
+  return (
+    <form 
+        className='searchbar'
+        onSubmit={handleSearch}>
+       <div className="searchbar__item">
+          <SearchManufacturer 
+             manufacturer={manufacturer}
+             setManufacturer={setManufacturer}/>
+       </div> 
+
+    </form>
+  )
+}
+
+export default SearchBar
